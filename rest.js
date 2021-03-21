57dff5246f6976532114c5f595795af9be3f8ba2
bsaliba <baptsal@gmail.com>
2021-03-21 02:25:29 -0400

    Using blockchain/info instead of coinapi

--- rest.js
+++ rest.js
@@ -2,12 +2,9 @@ const https = require('https');
 
 let options = {
   method: 'GET',
-  hostname: 'rest.coinapi.io',
-  path: '/v1/exchangerate/BTC/USD?time=2021-03-20T00:00:00',
+  hostname: 'blockchain.info',
+  path: '/tobtc?currency=USD&value=5000',
   port: 443,
-  headers: {
-    'X-CoinAPI-Key': "2995043B-4497-448F-A913-2C3A04584C6E"
-  }
 }
 
 const req = https.request(options, res => {
