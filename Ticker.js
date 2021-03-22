3df6c3aef5f40ac5ddecc9ff21bb1ad4805511e8
bsaliba <baptsal@gmail.com>
2021-03-21 22:59:31 -0400

    Completed the Ticker module

--- Ticker.js
+++ Ticker.js
@@ -1,9 +1,9 @@
 const WebSocket = require('ws');
-const api_url = 'wss://ws-feed.pro.coinbase.com';
+const API_URL = 'wss://ws-feed.pro.coinbase.com';
 const BITCOIN = "BTC-USD";
 const ETHEREUM = "ETH-USD"
-const initiation_message = `{
-    "type": "subscribe",
+const SUBSCRIPTION_MESSAGE = `{
+  "type": "subscribe",
     "product_ids": [
         "ETH-USD",
         "BTC-USD"
@@ -21,16 +21,38 @@ const initiation_message = `{
     ]
 }`
 
-function timeout(ms) { //pass a time in milliseconds to this function
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 class Ticker {
 
   constructor(){
     this.bitcoin = 0;
     this.ethereum = 0;
     this.retrieveExchangeRates();
+  }
+  
+  async retrieveExchangeRates() {
+    let client = new WebSocket(API_URL);
+  
+    // Wait for the client to connect using async/await
+    await new Promise(resolve => client.once('open', resolve));
+
+    // Whenever websocket sends a new message containing updated prices, update instance variables 
+    client.on('message', msg => this.update_prices(msg));
+  
+    // console.log("Connected to CoinBase Websocket")
+    client.send(SUBSCRIPTION_MESSAGE)
+  }
+
+  update_prices(message) {
+    let asset, price
+    [asset, price] = this.parse_message(message);
+    switch(asset){
+      case BITCOIN:
+        this.bitcoin = price;
+        break;
+      case ETHEREUM:
+        this.ethereum = price;
+        break;
+    }
   }
 
   parse_price(price_string) {
@@ -40,47 +62,16 @@ class Ticker {
   parse_time(time_string) {
     return new Date(time_string)
   }
-  
+
   parse_message(raw_message) {
     let message = JSON.parse(raw_message)
     if (message["type"] == "ticker") {
       let asset = message["product_id"];
       let price = this.parse_price(message["price"]);
-      let time = this.parse_time(message["time"]);
-      switch(asset){
-        case BITCOIN:
-          this.bitcoin = price;
-          break;
-        case ETHEREUM:
-          this.ethereum = price;
-          break;
-      }
-      // Live feed of websocket output
-      // let format_output = `Asset: ${asset}| Price: ${price}| Time: ${time}`;
-      // console.log(format_output);
+      return([asset, price])
     }
-  }
-  
-  async retrieveExchangeRates() {
-  
-    let client = new WebSocket(api_url);
-  
-    client.on('message', msg => this.parse_message(msg));
-  
-    // Wait for the client to connect using async/await
-    await new Promise(resolve => client.once('open', resolve));
-  
-    console.log("Connected to CoinBase Websocket")
-    client.send(initiation_message)
+    return [undefined, undefined]
   }
 }
 
-async function test() {
-  await timeout(5000);
-  console.log(ticker.bitcoin);
-  console.log(ticker.ethereum);
-}
-
-// module.exports = {Ticker}
-let ticker = new Ticker();
-test();
+module.exports = {Ticker}
